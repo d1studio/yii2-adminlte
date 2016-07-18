43b22a28bca3eaae5fe062a604364ae466cc4e12
程涛 <751753158@qq.com>
2016-07-18 16:32:44 +0800

    添加adminlte asset

---
+++ AdminLetAsset.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace d1studio\yii2_adminlet;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+use \Exception;
+
+/**
+ * AdminLte AssetBundle
+ * @since 0.1
+ */
+class AdminLteAsset extends AssetBundle
+{
+    /**
+     * @inherit
+     */
+    public $sourcePath = '@vendor/d1studio/yii2-adminlte/assets';
+    public $basePath   = '@webroot';
+    public $baseUrl    = '@web';
+    public $css = [
+        'dist/css/AdminLTE.min.css',
+    ];
+    public $js = [
+        'dist/js/app.min.js',
+    ];
+    public $jsOptions = [
+        'position'=>View::POS_HEAD
+    ];
+    public $depends = [
+        'rmrevin\yii\fontawesome\AssetBundle',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'yii\web\JqueryAsset',
+    ];
+    /**
+     * @var string|bool Choose skin color, eg. `'skin-blue'` or set `false` to disable skin loading
+     * @see https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
+     */
+    public $skin = '_all-skins';
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        // Append skin color file if specified
+        if ($this->skin) {
+            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
+                throw new Exception('Invalid skin specified');
+            }
+            $this->css[] = sprintf('dist/css/skins/%s.min.css', $this->skin);
+        }
+        parent::init();
+    }
+}
